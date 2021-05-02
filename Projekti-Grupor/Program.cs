03f3d05c6788c304b12fd92f62177af89c5f7e9f
Lisi1920 <73655859+Lisi1920@users.noreply.github.com>
2021-05-02 18:57:45 +0200

    UPdated text

--- Projekti-Grupor/Program.cs
+++ Projekti-Grupor/Program.cs
@@ -13,8 +13,12 @@ namespace Projekti_Grupor
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("Hello World!!!");
         }
+        
+
+       
+        
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
